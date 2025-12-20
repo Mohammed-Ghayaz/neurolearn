c2aec89bfbfbbbd9e6917559025fabff75b46157
Mohammed-Ghayaz <mghayaz04@gmail.com>
2025-12-20 14:08:36 +0530

    User model created

---
+++ backend/src/models/user_model.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, DateTime, Enum as SQLAlchemyEnum
+from sqlalchemy.dialects.postgresql import UUID
+from ..db.database import Base
+from datetime import datetime, timezone
+from enum import Enum
+import uuid
+
+class UserRole(str, Enum):
+    STUDENT = "student"
+    PARENT = "parent"
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(UUID(as_uuid=True), primary_key=True, index=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password_hash = Column(String, nullable=False)
+    role = Column(SQLAlchemyEnum(UserRole), nullable=False, default=UserRole.STUDENT.value)
+    age = Column(Integer, nullable=True)
+    created_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc))
+    updated_at = Column(DateTime(timezone=True), default=lambda: datetime.now(timezone.utc), onupdate=datetime.now(timezone.utc))
